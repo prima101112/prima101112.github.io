1214969e8b8e03d311b7071909ab2c56ff75fffe
prima <prima101112@gmail.com>
2015-09-11 22:00:17 +0700

    taggenerator

--- _plugins/tagGenerator.rb
+++ _plugins/tagGenerator.rb
@@ -13,7 +13,6 @@ module Jekyll
       self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
     end
   end
-
   class TagGenerator < Generator
     safe true
     def generate(site)
@@ -24,7 +23,6 @@ module Jekyll
         end
       end
     end
-
     def write_tag_index(site, dir, tag)
       index = TagIndex.new(site, site.source, dir, tag)
       index.render(site.layouts, site.site_payload)
@@ -32,5 +30,4 @@ module Jekyll
       site.pages << index
     end
   end
-
 end
